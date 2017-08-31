a414b2d434ba260617edd67ff83aeccbbabae9ce
Bisera Belkoska <Bisera.Belkoska@netcetera.com>
2017-08-31 12:18:08 +0200

    Initial Commit

---
+++ HotelManagement/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HotelManagement
+//
+//  Created by Bisera Belkoska on 31.08.17.
+//  Copyright © 2017 Bisera Belkoska. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
